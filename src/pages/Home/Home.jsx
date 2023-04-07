b771adf83cf9e269c0d190ce3092ab72c3063d21
Priyanshu85 <pg7909290@gmail.com>
2023-04-07 10:43:24 +0530

    Added Hero Animations

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Home.css'
 
 const Home = () => {
-  return (
-    <div className='h-[80vh]'><iframe src="https://app.vectary.com/p/2GIpXyydFjxeHls3lAJepM" frameborder="0" width="100%" height="650"></iframe></div>
-  )
-}
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
 
-export default Home
+  const handleMouseMove = (event) => {
+    setCoordinates({ x: event.clientX, y: event.clientY });
+  };
+
+  const renderUdbhavTexts = () => {
+    const udbhavTexts = [];
+
+    for (let i = 0; i < 50; i++) {
+      const randomX = Math.floor(Math.random() * window.innerWidth);
+      const randomY = Math.floor(Math.random() * window.innerHeight);
+      const randomDelay = Math.floor(Math.random() * 5);
+      udbhavTexts.push(
+        <div
+          key={i}
+          className="udbhav-text"
+          style={{
+            top: randomY,
+            left: randomX,
+            animationDelay: `${randomDelay}s`,
+          }}
+        >
+          UDBHAV
+        </div>
+      );
+    }
+    return udbhavTexts;
+  };
+
+  return (
+    <div className='h-[80vh]'>
+    <div className="h-full w-full fixed top-0 left-0" onMouseMove={handleMouseMove}>
+      {renderUdbhavTexts()}
+      <div
+        className="udbhav-text-hover"
+        style={{ top: coordinates.y, left: coordinates.x }}
+      >
+        UDBHAV2k23
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Home;
